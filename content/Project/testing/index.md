d91516a93684f2b56aae0ea430ac9ec349168eb4
Julian Fortune <fortunej@oregonstate.edu>
2020-08-20 20:04:44 -0700

    Removes source code

--- content/Project/testing/index.md
+++
@@ -1,5 +0,0 @@
----
-title: "Example Project"
----
-
-Itatur? Quiatae cullecum rem ent aut odis in re eossequodi nonsequ idebis ne sapicia is sinveli squiatum, core et que aut hariosam ex eat.
--- themes/julian/assets/js/main.js
+++
@@ -1,9 +0,0 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function toggleMenu() {
-    var x = document.getElementById("navigation");
-    if (x.className === "") {
-      x.className = "open";
-    } else {
-      x.className = "";
-    }
-  }
